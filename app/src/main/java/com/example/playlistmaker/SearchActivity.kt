1c1b5150a028885126fc3d36ab66467fcc11a119
AnShok <Shokin3111@gmail.com>
2023-08-27 14:23:52 +0300

    Выполнена работа по recycle view и библиотеке glide.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -10,8 +10,16 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-//import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
+
+class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String
+)
 class SearchActivity : AppCompatActivity() {
 
     private companion object {
@@ -19,6 +27,9 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private lateinit var inputEditText: EditText //глобальная переменная для EditText
+    private lateinit var clearButton: ImageView
+    private lateinit var recyclerView: RecyclerView
+
     private var textSearch: String = "" //глобальная переменная для хранения текста поискового запроса
 
 
@@ -28,10 +39,11 @@ class SearchActivity : AppCompatActivity() {
 
         //Нахождение элементов интерфейса
         inputEditText = findViewById(R.id.inputEditText) // инициализация inputEditText в onCreate
-        val backButton = findViewById<Button>(R.id.button_back)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        clearButton = findViewById(R.id.clearIcon)
+        recyclerView = findViewById(R.id.recycler_view)
         //val linearLayout = findViewById<LinearLayout>(R.id.containerSearch)
 
+        val backButton = findViewById<Button>(R.id.button_back)
 
         if (savedInstanceState != null) {
             textSearch = savedInstanceState.getString(TEXT_SEARCH,"") //Восстановление значения textSearch из сохраненного состояния
@@ -63,6 +75,11 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val trackList = generateTrackData()
+        val adapter = TrackAdapter(trackList)
+        recyclerView.adapter = adapter
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -84,5 +101,16 @@ class SearchActivity : AppCompatActivity() {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
     }
+
+    private fun generateTrackData(): List<Track> {
+        return listOf(
+            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
+            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
+            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
+            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
+            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
+        )
+    }
 }
+
 
